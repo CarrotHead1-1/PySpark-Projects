275a515c45abb66a22b5d8e132e5507b4fc90efc
up2109872 <up2109872@myport.ac.uk>
2025-08-29 14:50:23 +0100

    resetting the directory

--- Customer-base-estimation/utils/sparkSession.py
+++
@@ -1,6 +0,0 @@
-
-from pyspark.sql import SparkSession
-
-#returns a spark session 
-def get_spark(app_name: str = "InjestJiob") -> SparkSession:
-    return SparkSession.builder.appName(app_name).getOrCreate()
